68f5e00fd6210afebf903ec24dc72b67b7efa3c7
Panxicun <93714236+Panxicun@users.noreply.github.com>
2022-01-25 18:28:13 +0800

    L1-024 后天.c

---
+++ L1-024 后天.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int D;
+    scanf("%d",&D);
+    if(D<=5)
+    {
+        printf("%d",D+2);
+    }
+    else if(D==6)
+        printf("1");
+    else if(D==7)
+        printf("2");
+return 0;
+}
